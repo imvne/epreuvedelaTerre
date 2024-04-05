e8798152f4657e6aab3deabbd946c1ea44109784
imvne <imanebeaufils19@gmail.com>
2024-04-05 18:20:06 +0200

    Nom du programme et afficheur d'arguments

---
+++ terre01.rb
@@ -0,0 +1,5 @@
+#Nom du programme
+
+nom_fichier = File.basename(__FILE__) #basename permet d'éxtraire la dernière étape du chemin de mon fichier, donc le nom du fichier
+
+print nom_fichier
---
+++ terre02.rb
@@ -0,0 +1,12 @@
+#Afficheur d'arguments
+
+phrase = gets.chomp # variable contenant la phrase récupérée dans le terminal
+
+phrase.each_char do |caracteres|  # chaque caractère de cette phrase est stocké temporairement dans caracteres
+    if caracteres == " " #pour chaque caractère si c'est un espace affiche un retour à la ligne
+        print "\n"
+    else # sinon (si le caractère n'est pas un espace et donc est une lettre ou symbole)
+        print caracteres + "" #imprime le caractère à la suite
+    end
+end
+

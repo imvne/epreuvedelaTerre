8e4f064ebb4fb695afc8a5f7d77501b219bf800b
imvne <imanebeaufils19@gmail.com>
2024-04-05 20:21:48 +0200

    modifs alphabet

--- terre00.rb
+++ terre00.rb
@@ -1,14 +1,16 @@
+#Alphabet
+
 alphabet = ["a","z","e","r","t","y","u","i","o","p","q","s","d","f","g","h","j","k","l","m","w","x","c","v","b","n"]
 
 alphabet.sort!
 
-index_lettre = alphabet.index(alphabet[0]) #je met dans une variable l'index du premier élément
+index_lettre = alphabet.index(alphabet[0])       # je met dans une variable l'index du premier élément
  
 
  print alphabet[index_lettre]
 
 begin
-    index_lettre = index_lettre + 1 #je met dans la variable, la variable + 1
+    index_lettre = index_lettre + 1              # je met dans la variable, la variable + 1
     print alphabet[index_lettre]
 end while index_lettre != 25
 

75140f6907eab47338ee9b9a774ea3c8ba73e4c3
imvne <imanebeaufils19@gmail.com>
2024-04-05 20:22:37 +0200

    modifs afficheur d'arguments

--- terre02.rb
+++ terre02.rb
@@ -1,12 +1,13 @@
 #Afficheur d'arguments
 
-phrase = gets.chomp # variable contenant la phrase récupérée dans le terminal
+phrase = gets.chomp                  # variable contenant la phrase entrée dans le terminal par l'utilisateur
 
-phrase.each_char do |caracteres|  # chaque caractère de cette phrase est stocké temporairement dans caracteres
-    if caracteres == " " #pour chaque caractère si c'est un espace affiche un retour à la ligne
+phrase.each_char do |caracteres|     # chaque caractère de cette phrase est stocké temporairement dans caracteres
+    if caracteres == " "             # pour chaque caractère si c'est un espace affiche un retour à la ligne
         print "\n"
-    else # sinon (si le caractère n'est pas un espace et donc est une lettre ou symbole)
-        print caracteres + "" #imprime le caractère à la suite
+    else                             # sinon (si le caractère n'est pas un espace et donc est une lettre ou symbole)
+        print caracteres + ""        # imprime le caractère à la suite
     end
 end
 
+print "\n"                           # pour éviter le %

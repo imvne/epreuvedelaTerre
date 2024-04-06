454cf9fac42187bdad5c6e5981b512cd3afbb22f
imvne <imanebeaufils19@gmail.com>
2024-04-06 19:50:42 +0200

    modif alphabet à partir de

--- terre03.rb
+++ terre03.rb
@@ -5,9 +5,10 @@ lettre = gets.chomp                              # variable contenant la lettre 
 print lettre                                     # affiche la lettre entrée
 print lettre.succ                                # imprime la lettre suivante
 
+
 begin 
     lettre = lettre.succ               # à chaque tour de la boucle on remplacera le contenu de la variable par la lettre suivante de l'alphabet
     print lettre.succ                  # à chaque tour de la boucle on affiche la lettre suivante de la dernière lettre suivante
-end while lettre.succ != "z"           # on continue la boucle tant que la lettre suivante n'est pas z
+end while lettre.succ != "z"         # on continue la boucle tant que la lettre suivante n'est pas z
 
 print "\n"                             # revient à la ligne

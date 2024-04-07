f4e827e028751b0ce3ca73e29e9fcbfe7e443f25
imvne <imanebeaufils19@gmail.com>
2024-04-07 13:51:12 +0200

    modifs inverser une chaîne

--- terre06.rb
+++ terre06.rb
@@ -1,20 +1,17 @@
 # Inverser une chaîne
 
-input = gets.chomp 
+input = gets.chomp                                   # variable contenant les deux nombres entrés dans le terminal par l'utilisateur
 
-chaine = input.chars
-index = chaine.index(chaine[-1])
+chaine = input.chars                                 # la chaine de caractère est stockée en caractères individuels dans la liste chaine
+index = chaine.length - 1                            # variable contenant l'index du dernier élément de la liste
 
-if input.empty?
+if input.empty?                                      # si l'utilisateur n'entre rien affiche ce message d'erreur
     print "erreur : aucun argument fourni"
 else
-begin
-    print chaine[index]
-    index = index - 1
-    if chaine[index] == " "   
-        print ""          
-    end
-end while index != -1
+while   index >= 0  do                               # sinon, tant que l'index est supérieur ou égal à 0 (premier élément du tableau)
+    print chaine[index]                              # affiche le dernier élément de la liste chaine
+    index -= 1                                       # affiche l'élément précédant le dernier élément de la liste chaine
+end 
 end
 
 print "\n"

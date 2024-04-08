de9cd16f1bd6f5c95af5ed1110f7ae5945c2928c
imvne <imanebeaufils19@gmail.com>
2024-04-08 22:35:38 +0200

    puissance d'un nombre

---
+++ terre08.rb
@@ -0,0 +1,24 @@
+# Puissance d'un nombre
+
+arguments = ARGV
+
+nombre = ARGV[0].to_i        # variable nombre contenant le premier élément des arguments donnés, converti en integer
+exposant = ARGV[1].to_i      # variable exposant contenant le deuxième élément des arguments donnés, converti en integer
+
+résultat = 1 
+
+
+if arguments.length != 2                            # si l'utilisateur n'entre rien ou + d'un argument 
+    puts "erreur : insérer seulement 2 arguments"   # affiche ce message d'erreur
+elsif ARGV[0] =~ /\D/ || ARGV[1] =~ /\D/            # si l'utilisateur entre autre chose que des nombres
+    puts "erreur : insérer seulement des nombres"   # affiche ce message d'erreur
+else
+    exposant.times do                               # sinon fait ça "exposant" fois
+    résultat *= nombre                              # multiplie le nombre par lui même
+    end                                
+    puts résultat                                   # affiche le résultat
+end
+
+
+# j'attribue 1 à la variable résultat, afin que la première mutliplication soit égale au nombre donné
+# pour ensuite mutliplier ce nombre donné à lui même "exposant" fois

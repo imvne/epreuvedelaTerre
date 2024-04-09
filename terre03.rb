53d79ff8e3165c1affab1ff52f241d2fb7dc759f
imvne <imanebeaufils19@gmail.com>
2024-04-09 12:49:15 +0200

    optimisation l'alphabet à partir de

--- terre03.rb
+++ terre03.rb
@@ -1,17 +1,18 @@
 # L'alphabet à partir de
 
-input = gets.chomp                               # variable contenant la lettre entrée dans le terminal par l'utilisateur
+argument = ARGV                                      # variable contenant la lettre entrée dans le terminal par l'utilisateur
 
-lettre = input.downcase                          # met la lettre en minuscule si elle a été entrée en majuscule
+lettre = ARGV[0].downcase                            # met la lettre en minuscule
 
-print lettre                                     # affiche la lettre entrée
 
-unless lettre == "z"                             # à moins que la lettre entrée soit z (pas de suite)
-begin 
-    lettre = lettre.succ                         # à chaque tour de la boucle on remplacera le contenu de la variable lettre par la lettre suivante de l'alphabet
-    print lettre                                 # à chaque tour de la boucle on affiche la lettre suivante
-
-end while lettre != "z"                          # on continue la boucle tant que la dernière lettre affichée n'est pas z    
+if argument.length != 1 || ARGV[0].length != 1 || lettre =~ /[^a-z]/      # si l'utilisateur n'entre rien ou + d'un argument, ou un argument avec plus d'une lettre, ou autre qu'une lettre
+    puts "erreur : n'insérer qu'une lettre"                               # affiche ce message d'erreur
+else
+    print lettre                    
+    until lettre == "z"                              # jusqu'à ce que la lettre suivante affichée soit z
+        lettre = lettre.succ                         # à chaque tour de la boucle on remplacera le contenu de la variable lettre par la lettre suivante de l'alphabet
+        print lettre                                 # à chaque tour de la boucle on affiche la lettre suivante
+    end  
 end
 
-print "\n"                                       # revient à la ligne
+puts "\n"

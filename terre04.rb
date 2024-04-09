5598b345b51b6da4c5d904e5eb9fa1ef70d2fe93
imvne <imanebeaufils19@gmail.com>
2024-04-09 13:34:36 +0200

    modifs pair ou impair

--- terre04.rb
+++ terre04.rb
@@ -2,9 +2,9 @@
 
 argument = ARGV                                      # variable contenant l'élément entré dans le terminal par l'utilisateur
 
-nombre = ARGV[0].to_i
+nombre = ARGV[0].to_i                                # variable contenant le premier élément de l'argument transformé en integer
 
-if argument.length != 1 || nombre =~ /^-?\d+$/ || ARGV[0] =~ /\D/       # si l'élément entré est un nombre entier, négatif ou non 
+if argument.length != 1 || nombre =~ /^-?\d+$/ || ARGV[0] =~ /\D/       # si l'utilisateur n'entre rien ou + d'un argument, ou autre qu'un nombre
     puts "Tu ne me la mettra pas à l'envers"
 else
     if nombre % 2 == 0                              # si la divison nombre / 2 à pour reste 0

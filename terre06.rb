a4cb29d2afd0a8c195c1e27564887a46c8ae1c44
imvne <imanebeaufils19@gmail.com>
2024-04-09 14:56:56 +0200

    optimisation inverser une chaîne

--- terre06.rb
+++ terre06.rb
@@ -1,18 +1,19 @@
 # Inverser une chaîne
 
-input = gets.chomp                                   # variable contenant les deux nombres entrés dans le terminal par l'utilisateur
+argument = ARGV                
 
-chaine = input.chars                                 # la chaine de caractère est stockée en caractères individuels dans la liste chaine
-index = chaine.length - 1                            # variable contenant l'index du dernier élément de la liste
 
-if input.empty?                                      # si l'utilisateur n'entre rien affiche ce message d'erreur
-    print "erreur : aucun argument fourni"
+if argument.length != 1                                  # si l'utilisateur n'entre rien ou plus d'un argument
+    print "erreur : insérer un argument"                 # affiche ce message d'erreur
 else
-while   index >= 0  do                               # sinon, tant que l'index est supérieur ou égal à 0 (premier élément du tableau)
-    print chaine[index]                              # affiche le dernier élément de la liste chaine
-    index -= 1                                       # affiche l'élément précédant le dernier élément de la liste chaine
-end 
+
+    chaine = ARGV[0].chars                               # l'argument est stockée en caractères individuels dans la liste chaine
+    index = chaine.length - 1                            # variable contenant l'index du dernier élément de la liste
+    
+    while   index >= 0  do                               # sinon, tant que l'index est supérieur ou égal à 0 (premier élément du tableau)
+        print chaine[index]                              # affiche le dernier élément de la liste chaine
+        index -= 1                                       # affiche l'élément précédant le dernier élément de la liste chaine
+    end 
 end
 
 print "\n"
-

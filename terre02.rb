57379058925a7eb48d5b2b64d005b52f7a6cf5c6
imvne <imanebeaufils19@gmail.com>
2024-04-09 12:06:00 +0200

    optimisation afficheur d'arguments

--- terre02.rb
+++ terre02.rb
@@ -1,13 +1,7 @@
 #Afficheur d'arguments
 
-phrase = gets.chomp                  # variable contenant la phrase entrée dans le terminal par l'utilisateur
+arguments = ARGV                    # variable contenant la phrase entrée dans le terminal par l'utilisateur, arguments séparés par les espaces
 
-phrase.each_char do |caracteres|     # chaque caractère de cette phrase est stocké temporairement dans caracteres
-    if caracteres == " "             # pour chaque caractère si c'est un espace affiche un retour à la ligne
-        print "\n"
-    else                             # sinon (si le caractère n'est pas un espace et donc est une lettre ou symbole)
-        print caracteres + ""        # imprime le caractère à la suite
-    end
+arguments.each do |caracteres|     # chaque élément du tableau arguments est stocké temporairement dans caracteres
+    print caracteres + "\n"        # affiche chaque argument + un retour à la ligne
 end
-
-print "\n"                           # pour éviter le %

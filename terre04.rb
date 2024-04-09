46860bb1185718813d00b6396c8eb740e77b13c9
imvne <imanebeaufils19@gmail.com>
2024-04-09 13:25:17 +0200

    optimisation pair ou impair

--- terre04.rb
+++ terre04.rb
@@ -1,14 +1,15 @@
 # Pair ou impair ?
 
-input = gets.chomp                                  # variable contenant l'élément entré dans le terminal par l'utilisateur
+argument = ARGV                                      # variable contenant l'élément entré dans le terminal par l'utilisateur
 
-if  input.match?(/^-?\d+$/)                         # si l'élément entré est un nombre entier, négatif ou non 
-    nombre = input.to_i                             # transforme la chaine de caractères en un integer
+nombre = ARGV[0].to_i
+
+if argument.length != 1 || nombre =~ /^-?\d+$/ || ARGV[0] =~ /\D/       # si l'élément entré est un nombre entier, négatif ou non 
+    puts "Tu ne me la mettra pas à l'envers"
+else
     if nombre % 2 == 0                              # si la divison nombre / 2 à pour reste 0
         puts "pair"                                 # alors affiche que c'est un nombre pair
     else
         puts "impair"                               # sinon affiche que c'est un nombre impair
     end
-else                                                # si l'élément entré n'est pas un nombre entier, négatif ou non 
-    puts "Tu ne me la mettra pas à l'envers"        # alors affiche ceci
 end

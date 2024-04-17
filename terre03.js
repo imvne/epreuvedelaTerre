81ed94feb3d8e97bc3b4c5db4a39214b64ec4251
imvne <imanebeaufils19@gmail.com>
2024-04-17 15:51:15 +0200

    sans fonctions alphabet à partir de

--- terre03.js
+++ terre03.js
@@ -4,40 +4,24 @@ let arguments = process.argv;
 
 let letter = process.argv[2];
 
-function isThereanArg() {
-  if (!letter) {
-    return "erreur : insérez une lettre";
-  } else if (letter.length > 1) {
-    return "erreur : n'insérez qu'une lettre";
-  }
-}
-
-function isItLetter() {
-  if (!/^[a-zA-Z]+$/.test(letter)) {
-    return "erreur : n'insérez rien d'autre qu'une lettre";
-  }
-}
-
 let alphabet = "";
 
-function aPartirDe() {
-  if (isThereanArg()) {
-    console.log(isThereanArg());
-  } else if (isItLetter()) {
-    console.log(isItLetter());
-  } else if (/^[a-z]+$/.test(letter)) {
-    let codeAscii = letter.charCodeAt(0);
-    for (; codeAscii < 123; codeAscii++) {
-      alphabet += String.fromCharCode(codeAscii);
-    }
-    console.log(alphabet);
-  } else if (/^[A-Z]+$/.test(letter)) {
-    let codeAscii = letter.charCodeAt(0);
-    for (; codeAscii < 91; codeAscii++) {
-      alphabet += String.fromCharCode(codeAscii);
-    }
-    console.log(alphabet);
+if (!letter) {
+  console.log("erreur : insérez une lettre");
+} else if (letter.length > 1) {
+  console.log("erreur : n'insérez qu'une lettre");
+} else if (!/^[a-zA-Z]+$/.test(letter)) {
+  console.log("erreur : n'insérez rien d'autre qu'une lettre");
+} else if (/^[a-z]+$/.test(letter)) {
+  let codeAscii = letter.charCodeAt(0);
+  for (; codeAscii < 123; codeAscii++) {
+    alphabet += String.fromCharCode(codeAscii);
   }
+  console.log(alphabet);
+} else if (/^[A-Z]+$/.test(letter)) {
+  let codeAscii = letter.charCodeAt(0);
+  for (; codeAscii < 91; codeAscii++) {
+    alphabet += String.fromCharCode(codeAscii);
+  }
+  console.log(alphabet);
 }
-
-aPartirDe();
